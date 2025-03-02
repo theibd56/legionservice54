db69729c6c99bee4952967696768366342782fd6
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2025-03-02 11:50:14 +0700

    breadcrumbs has been created

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
+                catalog: resolve(__dirname, '/pages/catalog.html'),
             },
         },
     },
