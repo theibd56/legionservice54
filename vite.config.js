55cd7435c46eb94f6e959ea9ca2f7363f59be437
theibd56 <igorbezhinarov@yandex.ru>
2025-03-19 14:59:43 +0700

    a little bit fix

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+    base: './',
     build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                catalog: resolve(__dirname, '/pages/_catalog.html'),
-                about: resolve(__dirname, '/pages/_about.html'),
-                cart: resolve(__dirname, '/pages/_cart.html'),
-                cartEmpty: resolve(__dirname, '/pages/_cart-empty.html'),
-                product: resolve(__dirname, '/pages/_product.html'),
+                catalog: resolve(__dirname, 'pages/_catalog.html'),
+                about: resolve(__dirname, 'pages/_about.html'),
+                cart: resolve(__dirname, 'pages/_cart.html'),
+                cartEmpty: resolve(__dirname, 'pages/_cart-empty.html'),
+                product: resolve(__dirname, 'pages/_product.html'),
             },
         },
     },
@@ -20,4 +21,4 @@ export default defineConfig({
             partialDirectory: resolve(__dirname, 'partials'),
         }),
     ],
-})
+});
